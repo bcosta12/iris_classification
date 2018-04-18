f7189898d91fa443192ab76796451f779792c898
Bernardo Costa <bernardoantunescosta@gmail.com>
2018-04-18 01:35:15 -0300

    Training

--- iris_classification.py
+++ iris_classification.py
@@ -17,6 +17,15 @@ def parse_csv(line):
   label = tf.reshape(parsed_line[-1], shape=())
   return features, label
 
+def loss(model, x, y):
+  y_ = model(x)
+  return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
+
+def grad(model, inputs, targets):
+  with tfe.GradientTape() as tape:
+    loss_value = loss(model, inputs, targets)
+  return tape.gradient(loss_value, model.variables)
+
 #Download the datasets
 train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
 train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url), origin=train_dataset_url)
@@ -37,3 +46,35 @@ model = tf.keras.Sequential([
   tf.keras.layers.Dense(10, activation="relu"),
   tf.keras.layers.Dense(3)
 ])
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+
+## Note: Rerunning this cell uses the same model variables
+
+# keep results for plotting
+train_loss_results = []
+train_accuracy_results = []
+
+num_epochs = 201
+
+for epoch in range(num_epochs):
+  epoch_loss_avg = tfe.metrics.Mean()
+  epoch_accuracy = tfe.metrics.Accuracy()
+
+  # Training loop - using batches of 32
+  for x, y in tfe.Iterator(train_dataset):
+    # Optimize the model
+    grads = grad(model, x, y)
+    optimizer.apply_gradients(zip(grads, model.variables),global_step=tf.train.get_or_create_global_step())
+
+    # Track progress
+    epoch_loss_avg(loss(model, x, y))  # add current batch loss
+    # compare predicted label to actual label
+    epoch_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
+
+  # end epoch
+  train_loss_results.append(epoch_loss_avg.result())
+  train_accuracy_results.append(epoch_accuracy.result())
+
+  if epoch % 50 == 0:
+    print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,epoch_loss_avg.result(),epoch_accuracy.result()))
